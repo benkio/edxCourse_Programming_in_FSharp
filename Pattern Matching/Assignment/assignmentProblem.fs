9fbcc06a961d1ef92b6dc2eb60d0d126a64776bd
Enrico Benini <benkio89@gmail.com>
2015-10-24 14:31:10 +0200

    Add formulas to the assignment

--- Pattern Matching/Assignment/assignmentProblem.fs
+++ Pattern Matching/Assignment/assignmentProblem.fs
@@ -1,26 +1,66 @@
 module assignmentProblem =
 (*
  * Assignment Problem on Pattern Matching
- * Staring from the Hands on practice idea
+ * Staring from the Hands on practice idea and https://en.wikipedia.org/wiki/Trajectory_of_a_projectile formulas
 *)
     open System
     open System.IO
 
-    (*(Target x coordinate, Target y coordinate, speed, expected distance, name)*)
-    type Entity =
-        { X : int
-          Y : int
-          Speed : int
-          distanceExprected: int
+    // Usefull types for the assignment
+
+    //(Target x coordinate, Target y coordinate, speed, expected distance, name)
+    type Input =
+        { x : float
+          y : float
+          speed : float
+          expectedDistance: float
           name : String }
 
-    let calculateAngleOfReach gravity distance speed = 0.5 * Math.Asin(((gravity * distance)/Math.Pow(speed,2.0)))
+    type Output =
+      { distance : float
+        hitExpectedDistance : Boolean
+        angle2ReachDistance : float
+        hitTarget: Boolean
+        angle2ReachTarget : float }
 
-    // let calculateDistanceTraveled  = () / 
+// Utilities to calculate stuff and manage inputs
+
+    let gravity = 9.81
+
+    let calculateAngleOfReach distance speed = 0.5 * Math.Asin(((gravity * distance)/Math.Pow(speed,2.0)))
+
+    let calculateDistanceTraveled angle speed  = (Math.Pow(speed,2.0) * Math.Sin(2.0*angle)) / gravity
+
+    let projectleHeightAtDistance distance angle speed = ((distance * (Math.Tan angle)) - (gravity * Math.Pow(distance,2.0)))/(2.0 * Math.Pow((speed * (Math.Cos angle)),2.0))
+
+    let angleToReachTarget x y speed =
+      let denominator = gravity * x
+      let squareRoot = Math.Pow(speed,4.0) - ( gravity * ((gravity * Math.Pow(x,2.0)) + (2.0*y*Math.Pow(speed,2.0))))
+      if (denominator > 0.0 && squareRoot > 0.0 )
+        then
+          let angle1 = Math.Atan ((Math.Pow(speed,2.0) + squareRoot )/denominator)
+          let angle2 = Math.Atan ((Math.Pow(speed,2.0) - squareRoot )/denominator)
+          if (angle1 > 0.0) then angle1 else angle2
+        else
+          raise (System.DivideByZeroException(""))
 
     let GetFile =
         Console.Write("Enter the full path to the name of the input file: ")
         Console.ReadLine()
+
+//Active Patterns
+
+    let (|ReachDistance|NotReachDistance|) (input:Input) =
+      match input with
+        | x when x.expectedDistance = calculateDistanceTraveled (calculateAngleOfReach x.expectedDistance x.speed) x.speed -> ReachDistance
+        | _ -> NotReachDistance
+
+    let (|ReachTarget|NotReachTarget|) (input:Input) =
+      match input with
+        | x when x.y = projectleHeightAtDistance x.expectedDistance (calculateAngleOfReach x.expectedDistance x.speed) x.speed -> ReachTarget
+        | _ -> NotReachTarget
+
+//Pattern Matching
 
     [<EntryPoint>]
     let main argv =
@@ -36,12 +76,12 @@ module assignmentProblem =
                 [ while not input.EndOfStream do
                       let raw = input.ReadLine()
                       let values = raw.Split(',')
-                      yield { X = int values.[0]
-                              Y = int values.[1]
-                              Speed = int values.[2]
-                              distanceExprected = int values.[3]
+                      yield { x = float values.[0]
+                              y = float values.[1]
+                              speed = float values.[2]
+                              expectedDistance = float values.[3]
                               name = values.[4] } ]
-                                         
+            
             Console.ReadKey() |> ignore
             0
         with
