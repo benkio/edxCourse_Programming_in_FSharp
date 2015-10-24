36c9838337f937952b7770cd2f0e2aee9a7e8d51
Enrico Benini <benkio89@gmail.com>
2015-10-24 17:51:12 +0200

    add input file and finished the assignment, to be tested

--- Pattern Matching/Assignment/assignmentProblem.fs
+++ Pattern Matching/Assignment/assignmentProblem.fs
@@ -27,11 +27,20 @@ module assignmentProblem =
 
     let gravity = 9.81
 
-    let calculateAngleOfReach distance speed = 0.5 * Math.Asin(((gravity * distance)/Math.Pow(speed,2.0)))
+    let startingAngle x y = if x > 0.0 then Math.Atan (y/x) else raise (System.DivideByZeroException("starting angle"))
+
+    let calculateAngleOfReach distance speed =
+      if speed > 0.0
+      then 0.5 * Math.Asin(((gravity * distance)/Math.Pow(speed,2.0)))
+      else raise (System.DivideByZeroException("angle of reach"))
 
     let calculateDistanceTraveled angle speed  = (Math.Pow(speed,2.0) * Math.Sin(2.0*angle)) / gravity
 
-    let projectleHeightAtDistance distance angle speed = ((distance * (Math.Tan angle)) - (gravity * Math.Pow(distance,2.0)))/(2.0 * Math.Pow((speed * (Math.Cos angle)),2.0))
+    let projectleHeightAtDistance distance angle speed =
+      let denominator = (2.0 * Math.Pow((speed * (Math.Cos angle)),2.0))
+      if denominator > 0.0
+      then ((distance * (Math.Tan angle)) - (gravity * Math.Pow(distance,2.0)))/denominator
+      else raise (System.DivideByZeroException("height@distance"))
 
     let angleToReachTarget x y speed =
       let denominator = gravity * x
@@ -48,19 +57,53 @@ module assignmentProblem =
         Console.Write("Enter the full path to the name of the input file: ")
         Console.ReadLine()
 
+    let printOutput output =
+      printfn "distance: %f - hitExpectedDistance: %b - angle2ReachDistance: %f hitTarget: %b - angle2ReachTarget %f" output.distance output.hitExpectedDistance output.angle2ReachDistance output.hitTarget output.angle2ReachTarget
+
 //Active Patterns
 
     let (|ReachDistance|NotReachDistance|) (input:Input) =
       match input with
-        | x when x.expectedDistance = calculateDistanceTraveled (calculateAngleOfReach x.expectedDistance x.speed) x.speed -> ReachDistance
+        | x when x.expectedDistance = calculateDistanceTraveled (startingAngle x.x x.y) x.speed -> ReachDistance
         | _ -> NotReachDistance
 
     let (|ReachTarget|NotReachTarget|) (input:Input) =
       match input with
-        | x when x.y = projectleHeightAtDistance x.expectedDistance (calculateAngleOfReach x.expectedDistance x.speed) x.speed -> ReachTarget
+        | x when x.y = projectleHeightAtDistance x.x (startingAngle x.x x.y) x.speed -> ReachTarget
         | _ -> NotReachTarget
 
 //Pattern Matching
+
+    let processOutput (input:Input) : Output =
+      match input with
+        | ReachDistance ->
+          match input with
+            | ReachTarget ->
+              { distance = input.expectedDistance
+                hitExpectedDistance = true
+                angle2ReachDistance = startingAngle input.x input.y
+                hitTarget = true
+                angle2ReachTarget = 0.0 }
+            | NotReachTarget ->
+              { distance = input.expectedDistance
+                hitExpectedDistance = true
+                angle2ReachDistance = startingAngle input.x input.y
+                hitTarget = false
+                angle2ReachTarget = angleToReachTarget input.x input.y input.speed }
+        | NotReachDistance ->
+          match input with
+            | ReachTarget ->
+              { distance = calculateDistanceTraveled (startingAngle input.x input.y) input.speed
+                hitExpectedDistance = false
+                angle2ReachDistance =calculateAngleOfReach input.expectedDistance input.speed
+                hitTarget = true
+                angle2ReachTarget = 0.0 }
+            | NotReachTarget ->
+              { distance = calculateDistanceTraveled (startingAngle input.x input.y) input.speed
+                hitExpectedDistance = false
+                angle2ReachDistance = calculateAngleOfReach input.expectedDistance input.speed
+                hitTarget = false
+                angle2ReachTarget = angleToReachTarget input.x input.y input.speed }
 
     [<EntryPoint>]
     let main argv =
@@ -81,7 +124,11 @@ module assignmentProblem =
                               speed = float values.[2]
                               expectedDistance = float values.[3]
                               name = values.[4] } ]
-            
+
+            let outputs = List.map processOutput entities
+
+            List.iter printOutput outputs
+
             Console.ReadKey() |> ignore
             0
         with
